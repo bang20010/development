49152adbca9e54e6ddcd5dbddff1350f947db4d6
bang20010 <81549774+bang20010@users.noreply.github.com>
2022-11-28 13:47:44 +0900

    Update addCardPage.dart

--- cardApp/cardapp/lib/screen/addCardPage.dart
+++ cardApp/cardapp/lib/screen/addCardPage.dart
@@ -570,3 +570,10 @@ void popAddCard(final context, String filename) {
   filename = "";
   Navigator.pop(context);
 }
+ String rtnDocument(String companyName, String name, String position) {
+    String document = "";
+    if (companyName.isNotEmpty && name.isNotEmpty && position.isNotEmpty) {
+      document = "${companyName}_ ${name}_ ${position}";
+    }
+    return document;
+  }
